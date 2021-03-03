a6c533178d1541e385084c047163d55189815efe
Sonali Shukla <sonalikatara@gmail.com>
2021-03-02 21:01:00 -0800

    age should be an int

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const app = Vue.createApp({
     data(){
         return{
             name: 'Sonali',
-            age: '21',
+            age: 21,
             avatar: 'https://avatars.githubusercontent.com/u/4163671?s=460&u=871f50b0936f1d54d4fce5cae88b6c0b8757314a&v=4' 
         }
     },
