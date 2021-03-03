7ff50c92aec8d3ebc11662a815e168d3d34c4ae9
Sonali Shukla <sonalikatara@gmail.com>
2021-03-02 20:56:23 -0800

    create vue App

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-const app = Vue.createApp().mount('#assignment');
+const app = Vue.createApp({
+    data(){
+        return{
+            name: 'Sonali',
+            age: '21',
+            avatar: 'https://avatars.githubusercontent.com/u/4163671?s=460&u=871f50b0936f1d54d4fce5cae88b6c0b8757314a&v=4' 
+        }
+    },
+    methods: {
+        getRandomNumber(){
+            return Math.random();
+        }
+    }
+});
+app.mount('#assignment');
